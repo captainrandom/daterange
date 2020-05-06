33e9529388409a33d322cd179a3195e7f74929d3
Mark Jackson <mark.jackson@starz.com>
2020-05-06 04:27:28 -0700

    adding build action to upload to pipy.

--- setup.py
+++ setup.py
@@ -2,9 +2,18 @@ from setuptools import find_packages, setup
 
 setup(
     name='daterange.py',
-    version='1.0.0',
+    version='0.0.1',
     description='A cli too that generates a range of dates or timestamps',
     author='Mark Jackson',
+    url="https://https://github.com/captainrandom/daterange",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',  # TODO: need to come back and change this at some point
+        'Topic :: Software Development :: Build Tools',  # TODO: need to come back and change this at some point
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # TODO: need to come back and change this at some point
+        "Operating System :: OS Independent",
+    ],
     packages=find_packages(),
     entry_points={
         'console_scripts': ['daterange=date_range.daterange:main']
